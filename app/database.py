88a7a4037b8f280c89ae26bca34ec5aa26f2d10b
Jin Lowell Miranda <jelomiranda3@gmail.com>
2026-02-20 09:22:49 +0800

    Fix missing os import in database.py

--- app/database.py
+++ app/database.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:1234@localhost:5432/tooltracker"
+DATABASE_URL = os.environ.get("DATABASE_URL", "postgresql://postgres:admin123@localhost:5432/tooltracker")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
